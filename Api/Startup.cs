bb36036a4f738789fe7d6468cbcf29947c7a0a19
dave20100 <dave201000@gmail.com>
2021-01-12 18:54:50 +0100

    progress

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -31,6 +31,7 @@ namespace Api
                 options.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +45,8 @@ namespace Api
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'client';
+  title = 'The dating app';
   users: any;
 
   constructor(private http: HttpClient) {}
