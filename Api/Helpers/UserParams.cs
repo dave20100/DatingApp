e10fa09292b7ecef3964d218d08f3a4d66389182
Dawid Mikowski <mikowski@vialutions.com>
2021-04-14 14:59:19 +0200

    159

--- Api/Helpers/UserParams.cs
+++ Api/Helpers/UserParams.cs
@@ -14,5 +14,7 @@ namespace Api.Helpers
 
         public string CurrentUsername { get; set; }
         public string Gender { get; set; }
+        public int MinAge { get; set; } = 18;
+        public int MaxAge { get; set; } = 150;
     }
 }
