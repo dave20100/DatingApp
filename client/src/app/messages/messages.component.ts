9f19f71682f95b65e68cb7c469d934fe3341b5d1
Dawid Mikowski <mikowski@vialutions.com>
2021-05-18 16:17:06 +0200

    197

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -11,9 +11,10 @@ import { MessageService } from '../_services/message.service';
 export class MessagesComponent implements OnInit {
   messages: Message[];
   pagination: Pagination;
-  container = 'Outbox';
+  container = 'Unread';
   pageNumber = 1;
   pageSize = 5;
+  loading = false;
 
   constructor(private messageService: MessageService) { }
 
@@ -22,9 +23,11 @@ export class MessagesComponent implements OnInit {
   }
 
   loadMessages() {
+    this.loading = true;
     this.messageService.getMessages(this.pageNumber, this.pageSize, this.container).subscribe(response => {
       this.messages = response.result;
       this.pagination = response.pagination;
+      this.loading = false;
     })
   }
 
@@ -33,4 +36,9 @@ export class MessagesComponent implements OnInit {
     this.loadMessages();
   }
 
+  deleteMessage(id: number) {
+    this.messageService.deleteMessage(id).subscribe(() => {
+      this.messages.splice(this.messages.findIndex(m => m.id === id), 1)
+    })
+  }
 }
